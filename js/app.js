a54a8cf8e3ddbd4596fa95b0f769bc9662ce19cf
Ian O'Hara <ian.ohara96@gmail.com>
2021-05-05 19:58:19 -0500

    new try

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-const unityInstance = UnityLoader.instantiate("unityContainer", "Build/buildsWeb.json");
+const unityInstance = UnityLoader.instantiate("unityContainer", "Build/build.json");
 let isCameraReady = false;
 let isDetectionManagerReady = false;
 let gl = null;
