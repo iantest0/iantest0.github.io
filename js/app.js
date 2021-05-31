b81390023330a1b7944609089eed65019ac3cc5b
Ian O'Hara <ian.ohara96@gmail.com>
2021-05-31 13:19:27 -0500

    New UX

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-const unityInstance = UnityLoader.instantiate("unityContainer", "Build/mapBuild.json");
+const unityInstance = UnityLoader.instantiate("unityContainer", "Build/NewUIMap.json");
 let isCameraReady = false;
 let isDetectionManagerReady = false;
 let gl = null;
