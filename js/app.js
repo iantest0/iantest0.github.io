1f5ccc26a2eddbc7bb21bd617f6687d6ce9f1df1
Ian O'Hara <82839420+iantest0@users.noreply.github.com>
2022-03-23 23:36:30 -0500

    PLEASE

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-const unityInstance = UnityLoader.instantiate("unityContainer", "Build/newQR.json");
+const unityInstance = UnityLoader.instantiate("unityContainer", "Build/builds.json");
 let isCameraReady = false;
 let isDetectionManagerReady = false;
 let gl = null;
