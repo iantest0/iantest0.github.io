23b9ae9dbb839cb21bc85bbf8f58e90e228be7e0
iantest0 <ian.ohara96@gmail.com>
2022-12-02 12:39:38 -0500

    tersting map ar

--- UN/js/app.js
+++ UN/js/app.js
@@ -1,4 +1,4 @@
-const unityInstance = UnityLoader.instantiate("unityContainer", "Build/Withmap.json");
+const unityInstance = UnityLoader.instantiate("unityContainer", "Build/build.json");
 let isCameraReady = false;
 let isDetectionManagerReady = false;
 let gl = null;
