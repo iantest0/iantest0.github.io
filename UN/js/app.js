5744964db818d26e97e5efd23b575f1e589a3e48
iantest0 <ian.ohara96@gmail.com>
2022-12-02 12:33:36 -0500

    new  qr for map

--- UN/js/app.js
+++ UN/js/app.js
@@ -1,4 +1,4 @@
-const unityInstance = UnityLoader.instantiate("unityContainer", "Build/build.json");
+const unityInstance = UnityLoader.instantiate("unityContainer", "Build/Withmap.json");
 let isCameraReady = false;
 let isDetectionManagerReady = false;
 let gl = null;
